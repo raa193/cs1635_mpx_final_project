dac02fabab075efadab57563b4892d7558f2fd5e
TheoZervos <112411316+TheoZervos@users.noreply.github.com>
2025-12-02 13:23:19 -0500

    Update flight_list_view_model.dart
    Update the flight list view model for formatting and added fetchFlights for API usage

--- mpx/lib/viewmodels/flight_list_view_model.dart
+++ mpx/lib/viewmodels/flight_list_view_model.dart
@@ -5,14 +5,20 @@ import 'package:mpx/viewmodels/flight_view_model.dart';
 class FlightListViewModel extends ChangeNotifier {
   List<FlightViewModel> flights = <FlightViewModel>[];
 
+  Future<void> fectchFlights(String apiKey) async {
+    final results = await FlightService().fetchFlights(apiKey);
+
+    print("Fetched ${results.length} flights");
+
+    flights = results.map((flight) => FlightViewModel(flight: flight)).toList();
+}
+
   Future<void> fetchTestFlights() async {
     final results = await FlightService().fetchTestFlights();
 
     print("Fetched ${results.length} flights");
 
-    flights = results
-        .map((flight) => FlightViewModel(flight: flight))
-        .toList();
+    flights = results.map((flight) => FlightViewModel(flight: flight)).toList();
 
     notifyListeners();
   }
