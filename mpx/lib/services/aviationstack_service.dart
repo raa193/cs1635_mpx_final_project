251f65ce894f1732c1d2bed760a25b86e6d96004
TheoZervos <112411316+TheoZervos@users.noreply.github.com>
2025-12-02 01:40:49 -0500

    Added aviationstack usage
    Added services to interact with aviationstack

---
+++ mpx/lib/services/aviationstack_service.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:http/http.dart' as http;
+import 'package:mpx/models/flight.dart';
+
+class AviationStackService {
+  final String apiKey;
+
+  AviationStackService(this.apiKey);
+
+  Future<Map<String, dynamic>> getRawFlightInfo() async {
+    final url = Uri.parse(
+      'http://api.aviationstack.com/v1/flights'
+      '?access_key=$apiKey',
+    );
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load flight data: ${response.statusCode}');
+    }
+  }
+
+  Future<Map<String, dynamic>> loadTestFlightDataAsset() async {
+    final jsonString = await rootBundle.loadString('assets/flight_data.json');
+    return jsonDecode(jsonString)['data'];
+  }
+}
