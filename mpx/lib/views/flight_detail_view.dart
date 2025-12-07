bec26dc09ed8ec64d599161c5e69a80db9b5b9a4
Ryan Armendariz-Lopez <67991245+macentire@users.noreply.github.com>
2025-12-07 13:52:38 -0500

    Added Dummy Progress Bar
    need to fix padding issues on the detailed view

--- mpx/lib/views/flight_detail_view.dart
+++ mpx/lib/views/flight_detail_view.dart
@@ -3,6 +3,7 @@ import 'package:mpx/l10n/app_localizations.dart';
 import 'package:mpx/providers/locale_provider.dart';
 import 'package:mpx/viewmodels/flight_view_model.dart';
 import 'package:intl/intl.dart';
+import 'package:mpx/widgets/airplane_progress_bar.dart';
 import 'package:provider/provider.dart';
 
 class FlightDetailView extends StatelessWidget {
@@ -17,7 +18,7 @@ class FlightDetailView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          '${flight.airlineData.getName() ?? t.unknownAirline} ${t.flightNumber} ${flight.flightData.getFlightNumber() ?? "Erm"}',
+          '${flight.airlineData.getName() ?? t.unknownAirline} ${t.flightNumber} ${flight.flightData.getFlightNumber() ?? "-"}',
           style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Colors.blueAccent,
@@ -51,84 +52,126 @@ class FlightDetailView extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   // ~~~~~~~~~~ DEPARTURE INFORMATION ~~~~~~~~~~
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children:  [
-                          Text(
-                            flight.departureData.getIata() ?? '-',
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                            ),
+                  Expanded(
+                    flex: 2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // to coerce row to stay to the left of the screen
+                        SizedBox(
+                          width: double.infinity,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                flight.departureData.getIata() ?? '-',
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(width: 8),
+                              DecoratedBox(
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.grey,
+                                    width: 1.0,
+                                  ),
+                                  borderRadius: BorderRadius.circular(7.0),
+                                ),
+                                child: Padding(
+                                  padding: EdgeInsets.all(1),
+                                  child: Icon(Icons.flight_takeoff),
+                                ),
+                              ),
+                            ],
                           ),
-                          SizedBox(width: 8),
-                          DecoratedBox(
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.grey, width: 1.0),
-                              borderRadius: BorderRadius.circular(7.0),
-                            ),
-                            child: Padding(
-                              padding: EdgeInsets.all(1),
-                              child: Icon(Icons.flight_takeoff)
-                            ),
-                          )
-                        ],
-                      ),
-                      Text(
-                        '${t.terminal} ${flight.departureData.getTerminal() ?? '-'}',
-                      ),
-                      Text('${t.gate} ${flight.departureData.getGate() ?? '-'}'),
-                      Text(
-                        DateFormat('M/d/y\nHH:mm').format(
-                          flight.departureData.getScheduledDepart() ??
-                              DateTime(0000),
                         ),
-                        style: TextStyle(fontSize: 15, color: Colors.grey),
-                      ),
-                    ],
+                        Text(
+                          '${t.terminal} ${flight.departureData.getTerminal() ?? '-'}',
+                        ),
+                        Text(
+                          '${t.gate} ${flight.departureData.getGate() ?? '-'}',
+                        ),
+                        Text(
+                          DateFormat('M/d/y\nHH:mm').format(
+                            flight.departureData.getScheduledDepart() ??
+                                DateTime(0000),
+                          ),
+                          style: TextStyle(fontSize: 15, color: Colors.grey),
+                        ),
+                      ],
+                    ),
                   ),
-
-                  // ~~~~~~~~~~ ARRIVAL INFORMATION ~~~~~~~~~~
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Row(
-                        children: [
-                          DecoratedBox(
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.grey, width: 1.0),
-                              borderRadius: BorderRadius.circular(7.0),
-                            ),
-                            child: Padding(
-                              padding: EdgeInsets.all(1),
-                              child: Icon(Icons.flight_land)
-                            ),
+                  Expanded(
+                    flex: 3,
+                    child: 
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 12),
+                          child: FlightProgressBar(
+                            departure:
+                                flight.departureData.getScheduledDepart() ??
+                                DateTime.now(),
+                            arrival:
+                                flight.arrivalData.getScheduledArrival() ??
+                                DateTime.now().add(Duration(hours: 2)),
                           ),
-                          SizedBox(width: 8),
-                          Text(
-                            flight.arrivalData.getIata() ?? '-',
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Text(
-                        '${t.terminal} ${flight.arrivalData.getTerminal() ?? '-'}',
-                      ),
-                      Text('${t.gate} ${flight.arrivalData.getGate() ?? '-'}'),
-                      Text(
-                        DateFormat('M/d/y\nHH:mm').format(
-                          flight.arrivalData.getScheduledArrival() ??
-                              DateTime(0000),
                         ),
-                        style: TextStyle(fontSize: 15, color: Colors.grey),
-                        textAlign: TextAlign.right,
-                      ),
-                    ],
+          
+                  ),
+                  // ~~~~~~~~~~ ARRIVAL INFORMATION ~~~~~~~~~~
+                  Expanded(
+                    flex: 2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        //to coerce row to stay to the right of the screen
+                        SizedBox(
+                          width: double.infinity,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              DecoratedBox(
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.grey,
+                                    width: 1.0,
+                                  ),
+                                  borderRadius: BorderRadius.circular(7.0),
+                                ),
+                                child: Padding(
+                                  padding: EdgeInsets.all(1),
+                                  child: Icon(Icons.flight_land),
+                                ),
+                              ),
+                              SizedBox(width: 8),
+                              Text(
+                                flight.arrivalData.getIata() ?? '-',
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                textAlign: TextAlign.right,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text(
+                          '${t.terminal} ${flight.arrivalData.getTerminal() ?? '-'}',
+                        ),
+                        Text(
+                          '${t.gate} ${flight.arrivalData.getGate() ?? '-'}',
+                        ),
+                        Text(
+                          DateFormat('M/d/y\nHH:mm').format(
+                            flight.arrivalData.getScheduledArrival() ??
+                                DateTime(0000),
+                          ),
+                          style: TextStyle(fontSize: 15, color: Colors.grey),
+                          textAlign: TextAlign.right,
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
@@ -141,8 +184,7 @@ class FlightDetailView extends StatelessWidget {
                   style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                 ),
                 Text(
-                  flight.arrivalData.getBaggage() ??
-                      t.baggageInformationUnannounced,
+                  'Baggage: ${flight.arrivalData.getBaggage() ?? t.baggageInformationUnannounced}',
                 ),
               ],
             ),
