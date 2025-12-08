4d8a8cf182b37dd42f204f0a418a99d726920517
TheoZervos <112411316+TheoZervos@users.noreply.github.com>
2025-12-07 19:21:50 -0500

    Initial update for timezones in progress bar
    Switched to using timezones and UTC for an accurate flight progress bar.  Originally using local time which gave incorrect results

--- mpx/lib/widgets/airplane_progress_bar.dart
+++ mpx/lib/widgets/airplane_progress_bar.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:mpx/viewmodels/departure_data_view_model.dart';
+import 'package:mpx/viewmodels/arrival_data_view_model.dart';
 import 'dart:async';
 import 'dart:math' as math;
+import 'package:timezone/timezone.dart' as tz;
 
 class FlightProgressBar extends StatefulWidget {
-  final DateTime departure;
-  final DateTime arrival;
+  final DepartureDataViewModel departure;
+  final ArrivalDataViewModel arrival;
 
   const FlightProgressBar({
     required this.departure,
@@ -33,16 +37,42 @@ class _FlightProgressBarState extends State<FlightProgressBar> {
   }
 
   void _updateProgress() {
-    final now = DateTime.now();
-    final total = widget.arrival.difference(widget.departure).inSeconds;
-    final elapsed = now.difference(widget.departure).inSeconds;
+    final departLocation = tz.getLocation(
+      widget.departure.getTimezone() ?? 'UTC',
+    );
+    final arrivalLocation = tz.getLocation(
+      widget.arrival.getTimezone() ?? 'UTC',
+    );
 
-    if (elapsed <= 0)
+    final departTime = tz.TZDateTime.parse(
+      departLocation,
+      widget.departure.getActualDepart()?.toIso8601String() ??
+      widget.departure.getEstimatedDepart()?.toIso8601String() ??
+      widget.departure.getScheduledDepart()?.toIso8601String() ??
+          DateTime.now().toIso8601String(),
+    ).toUtc();
+
+    final arrivalTime = tz.TZDateTime.parse(
+      arrivalLocation,
+      widget.arrival.getActualArrival()?.toIso8601String() ??
+      widget.arrival.getEstimatedArrival()?.toIso8601String() ??
+      widget.arrival.getScheduledArrival()?.toIso8601String() ??
+          DateTime.now()
+              .add(Duration(hours: 2))
+              .toIso8601String(),
+    ).toUtc();
+
+    final now = DateTime.now().toUtc();
+    final total = arrivalTime.difference(departTime).inSeconds;
+    final elapsed = now.difference(departTime).inSeconds;
+
+    if (elapsed <= 0) {
       progress = 0;
-    else if (elapsed >= total)
+    } else if (elapsed >= total) {
       progress = 1;
-    else
+    } else {
       progress = elapsed / total;
+    }
   }
 
   @override
@@ -81,13 +111,14 @@ class _FlightProgressBarState extends State<FlightProgressBar> {
                 ),
               ),
               // Plane icon
-              Padding(
-                padding: EdgeInsets.only(left: 0, right: 0),
-                child: Positioned(
-                  left: width * progress,
-                  child: Transform.rotate(
-                    angle: math.pi / 2,
-                    child: Icon(Icons.local_airport, size: 24, color: Colors.grey),
+              Positioned(
+                left: width * progress,
+                child: Transform.rotate(
+                  angle: math.pi / 2,
+                  child: Icon(
+                    Icons.local_airport,
+                    size: 24,
+                    color: Colors.blueGrey,
                   ),
                 ),
               ),
